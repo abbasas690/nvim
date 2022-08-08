e1c33cbb3769430963478c9bd9c92a9437bd9627
abbasas690 <abbasas690@gmail.com>
2022-08-08 22:39:02 +0530

    lsp & filetype keymaps are fixed

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -10,13 +9,10 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map("n", ",<Space>", ":nohlsearch<CR>", { silent = true })
-map("n", "<Leader>x", ":q<CR>")
+map("n", ",<Space>", "<cmd>nohlsearch<CR>", { silent = true })
+map("n", "<Leader>q", "<cmd>q<CR>")
 map("i", "jj", "<ESC>")
-map("n", "<Leader>wq", ":wq<CR>")
-map("n", "<Leader>w", ":w<CR>")
---map("n", "<Leader>r", "<cmd>split term://make %< && ./%<<CR>")
+map("n", "<Leader>wq", "<cmd>wq<CR>")
+map("n", "<Leader>w", "<cmd>w<CR>")
 map("n", "<Leader>tt", "<cmd>lua toggletheme()<CR>")
 map("n", "<Leader>e", "<cmd>NvimTreeToggle<CR>")
-map("t", "<Esc>", "<C-/><C-n>",{noremap =true})
-map("n","<Leader>p","<cmd>split term://python % <CR>")
